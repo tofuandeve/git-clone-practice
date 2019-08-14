e8b9e971d23efd43472af3ea2dd76b2d43233ba9
Eve Le <nguyenthao.lee@gmail.com>
2019-08-14 10:11:12 -0700

    Testing GitHub account

--- hello.rb
+++ hello.rb
@@ -1,3 +1,4 @@
 puts "Hello World!"
 puts "I need to print this line because I was too excited and printed out 'Hello World' in the previous commmit"
 # this is a comment
+# testing my GitHub account
